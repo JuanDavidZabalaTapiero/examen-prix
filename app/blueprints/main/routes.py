6e0d04f804a0c814be1c6aa2ef71b33981e7f3ed
JuanDavidZabalaTapiero <juandavidzabalatapiero@gmail.com>
2025-09-06 20:56:32 -0500

    add: Gráficas de prueba

--- app/blueprints/main/routes.py
+++ app/blueprints/main/routes.py
@@ -1,8 +1,27 @@
 from flask import Blueprint, render_template
 
+from app.blueprints.questions.services import get_questions
+
 main_bp = Blueprint("main", __name__)
 
 
 @main_bp.route("/")
 def home():
-    return render_template("main/home.html")
+
+    questions = get_questions()
+
+    data = []
+
+    for q in questions:
+        labels = [opt.text for opt in q.options]
+        counts = [len(opt.responses) for opt in q.options]
+        data.append(
+            {
+                "question_id": q.id,
+                "question": q.text,
+                "labels": labels,
+                "counts": counts,
+            }
+        )
+
+    return render_template("main/home.html", data=data)
